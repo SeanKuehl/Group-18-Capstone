7e3c959ca8c666f1c3d30d965750513d27c3cb53
wardmason <masoncward@hotmail.ca>
2024-03-18 13:51:46 -0400

    Added making comment and making post functionality

--- Main/forms.py
+++ Main/forms.py
@@ -1,6 +1,39 @@
 from django import forms
 
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your Name"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Leave a comment!"}
+        )
+    )
+
 class PostForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    community = forms.CharField(max_length=100)
-    body = forms.CharField(widget=forms.Textarea)
+    username = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your Username"}
+        ),
+    )
+    post_title = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Title"}
+        ),
+    )
+    post_community = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Community"}
+        ),
+    )
+    post_body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Post body here..."}
+        )
+    )
--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from Main.models import Post, Comment
+from Main.forms import CommentForm, PostForm
 
 # Create your views here.
 from django.views.generic.base import TemplateView
@@ -10,9 +11,23 @@ class HomePage(TemplateView):
 
 def post_index(request):
     posts = Post.objects.all().order_by("-created_on")
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = Post(
+                username = form.cleaned_data["username"],
+                post_title = form.cleaned_data["post_title"],
+                post_community = form.cleaned_data["post_community"],
+                post_body = form.cleaned_data["post_body"]
+            )
+            post.save()
+            return HttpResponseRedirect(request.path_info)
     context = {
         "posts": posts,
+        "form": PostForm(),
     }
+
     return render(request, "index.html", context)
 
 def post_tag(request, tag):
@@ -27,10 +42,22 @@ def post_tag(request, tag):
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "detail.html", context)
