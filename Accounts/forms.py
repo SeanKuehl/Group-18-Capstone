bb25c412d89bead8507c3196170e1ef30515d6ab
Dominic Pham <dom17pham@gmail.com>
2024-03-19 21:27:14 -0400

    Removed custom authentication.
    Used Django built-in authentication module.

--- Accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class AccountLoginForm(AuthenticationForm):
-    class Meta:
-        fields = ['username', 'password']
