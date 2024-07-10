226ec139f869f2041c27c9e910ec065d9907b43c
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-10 11:45:57 -0400

    Update models.py

--- Accounts/models.py
+++ Accounts/models.py
@@ -10,6 +10,8 @@ class CustomUser(AbstractUser):
     reported_count = models.PositiveIntegerField(default=0)
     admin_status = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    steam_id = models.CharField(max_length=20, blank=True, null=True)
+    steam_name = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.username
