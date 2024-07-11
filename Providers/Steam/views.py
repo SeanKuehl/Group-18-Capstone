ae2bc835be740a180584975719d22f7c1ab29501
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 11:20:58 -0400

    Update views.py

--- Providers/Steam/views.py
+++ Providers/Steam/views.py
@@ -1,7 +1,7 @@
 from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView, OAuth2CallbackView
 
 class SteamOAuth2Adapter:
-    provider_id = SteamProvider.id
+    provider_id = 'steam'
     authorize_url = "https://steamcommunity.com/openid/login"
     access_token_url = "https://steamcommunity.com/openid/login"
     profile_url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
