e30f65d2ff807a1f91fcc89e1418b4cfd5b77fda
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 10:55:30 -0400

    Update views.py

--- Providers/Steam/views.py
+++ Providers/Steam/views.py
@@ -1,7 +1,7 @@
 from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView, OAuth2CallbackView
 
 class SteamOAuth2LoginView(OAuth2LoginView):
-    pass
+    view_name = "steam_login"
 
 class SteamOAuth2CallbackView(OAuth2CallbackView):
-    pass
+    view_name = "steam_callback"
