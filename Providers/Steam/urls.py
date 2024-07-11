b719a1c477a01302058a2558a62b4d69906b2dca
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 11:13:44 -0400

    Update urls.py

--- Providers/Steam/urls.py
+++ Providers/Steam/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 from Providers.Steam.provider import SteamProvider
+from django.contrib import admin
 
 urlpatterns = [
-    path('', include('Main.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('Main.urls')),  # Including the URLs from the Main application
 ]
 
-urlpatterns += default_urlpatterns(SteamProvider)
+from Providers.Steam.views import oauth2_login, oauth2_callback
+
+urlpatterns += [
+    path('accounts/steam/login/', oauth2_login, name='steam_login'),
+    path('accounts/steam/callback/', oauth2_callback, name='steam_callback'),
+]
