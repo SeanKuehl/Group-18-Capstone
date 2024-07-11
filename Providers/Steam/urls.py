d6b8447f2abb5ddfaf7af9e15edd4aea640f6208
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 11:00:27 -0400

    Update urls.py

--- Providers/Steam/urls.py
+++ Providers/Steam/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path, include
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-from .provider import SteamProvider
+from Providers.Steam.provider import SteamProvider
 
-urlpatterns = default_urlpatterns(SteamProvider)
+urlpatterns = [
+    path('', include('Main.urls')),
+]
+
+urlpatterns += default_urlpatterns(SteamProvider)
