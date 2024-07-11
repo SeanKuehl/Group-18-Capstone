3b9cdb53e1b624b0ff3c9eb42bfdb10b26bb92a2
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 10:43:26 -0400

    Small fix

---
+++ Providers/Steam/views.py
@@ -0,0 +1,7 @@
+from allauth.socialaccount.providers.oauth2.views import OAuth2LoginView, OAuth2CallbackView
+
+class SteamOAuth2LoginView(OAuth2LoginView):
+    pass
+
+class SteamOAuth2CallbackView(OAuth2CallbackView):
+    pass
