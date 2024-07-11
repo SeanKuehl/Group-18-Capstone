8e459da594af5651401bad85e27d87a12cf8cc67
Jess Vuksic <80832541+JessVuksic@users.noreply.github.com>
2024-07-11 11:30:01 -0400

    Update provider.py

--- Providers/Steam/provider.py
+++ Providers/Steam/provider.py
@@ -1,7 +1,5 @@
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.openid.provider import OpenIDProvider
-from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-from Providers.Steam.views import SteamOAuth2LoginView, SteamOAuth2CallbackView
 
 class SteamAccount(ProviderAccount):
     def to_str(self):
@@ -17,11 +15,5 @@ class SteamProvider(OpenIDProvider):
 
     def extract_extra_data(self, data):
         return data
-
-    def get_login_view(self):
-        return SteamOAuth2LoginView
-
-    def get_callback_view(self):
-        return SteamOAuth2CallbackView
 
 provider_classes = [SteamProvider]
